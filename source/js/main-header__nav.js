e5ff8b46ed96743d055998b135151c231db336ca
Elvira Fatin <elvirrrk@gmail.com>
2023-09-23 18:08:57 +0300

    fix: no-js mode

--- source/js/main-header__nav.js
+++ source/js/main-header__nav.js
@@ -1,4 +1,6 @@
-let toggle = document.querySelector('.toggle__input');
-toggle.checked = false;
-document.querySelector('.toggle__label').style.display = 'flex';
-document.querySelector('.main-nav').style.position = 'absolute';
+let toggleInput = document.querySelector('.toggle__input');
+toggleInput.checked = false;
+let toggleLabel = document.querySelector('.toggle__label');
+toggleLabel.classList.remove('toggle__label--no-js');
+let mainNav = document.querySelector('.main-nav');
+mainNav.classList.remove('main-nav--no-js');
